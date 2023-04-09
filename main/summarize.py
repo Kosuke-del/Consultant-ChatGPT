4a5bd52e4d8871d75fe0128a8fb36a8d8b4572b2
KosukeA <106922727+Kosuke-del@users.noreply.github.com>
2023-04-09 07:39:31 +0000

    summaraize.py_complete

--- main/summarize.py
+++ main/summarize.py
@@ -5,16 +5,15 @@ This is a file to summarize sentences.
 import openai
 
 text = input("要約したい文章を入力してください: ")
+max_length = input("要約した文章の最大長を入力してください: ")
 
 res = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Summarize these sentences in 50 words.「{text}」"},
+        {"role": "user", "content": f"Summarize these sentences in {max_length} words.「{text}」"},
     ]
 )
 res_content = res["choices"][0]["message"]["content"]
 
-with open("output.txt1", encoding='utf-8' "w") as f:
-    f.write(res_content)
-    
+print(f"要約した文章です: {res_content}")
